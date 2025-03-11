3829dafa53df589161f23a6b0fe117a5ec1ca25c
gebraic <febriodean@gmail.com>
2025-03-11 17:26:13 +0700

    perubahan

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -48,24 +48,24 @@ export default function DetailPokemon() {
         <div className= "bg-blue-400 min-h-screen w-screen text-center flex flex-col items-center justify-center p-3">
             <Typography variant="h1" className="text-gray-900 mb-2">{pokemon.name.toUpperCase()}</Typography>
             <div className="bg-amber-200 max-w-[85%] max-h-[75%] h-full w-full p-4 rounded-lg shadow-lg flex flex-col md:flex-row gap-4">
-                    <div item className="flex-1 flex flex-col items-center">
+                    <div item className="flex-1 flex flex-col items-center justify-center">
                         <Card className="p-4 rounded-lg shadow-md">
                             <LazyLoadImage 
-                            className="max-w-[auto] max-h-[250px] w-full h-full"
+                            className="max-w-[auto] max-h-[220px] w-full h-full"
                             src={pokemon.officialArtwork || "/placeholder.png"}
                             alt={pokemon.name} 
                             effect="blur" 
                             />
                         </Card>
                         <Slide in={true} direction="up" className="mt-4 flex items-center justify-center gap-2">
-                            <Box display="flex" alignItems="center" justifyContent="center" mt={2}>
+                            <Box mt={2} className="max-h-[40%] h-full w-auto">
                                 <IconButton onClick={handlePrevSprite}><ArrowBackIos /></IconButton>
                                 {spriteKeys.length > 0 && (
                                     <LazyLoadImage 
                                         src={pokemon.sprites[spriteKeys[currentSpriteIndex]]} 
                                         alt={`${pokemon.name} sprite`} 
                                         effect="blur"
-                                        className="max-h-[150px] max-w-[auto] h-full w-full"
+                                        className="max-h-[200px] max-w-[auto] h-full w-full"
                                     />
                                 )}
                                 <IconButton onClick={handleNextSprite}><ArrowForwardIos /></IconButton>
